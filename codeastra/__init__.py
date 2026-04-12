35af225d93f1a8928a7f48ae230fdf4fbd6e89dc
Dinzeyi2 <133053311+Dinzeyi2@users.noreply.github.com>
2026-04-11 20:45:59 -0400

    Create  __init__.py

---
+++ codeastra/__init__.py
@@ -0,0 +1,12 @@
+from .middleware import BlindAgentMiddleware
+from .client import CodeAstraClient
+from .wrappers import blind_tool, BlindCrewAIAgent, BlindAutoGPTAgent
+
+__version__ = "1.0.0"
+__all__ = [
+    "BlindAgentMiddleware",
+    "CodeAstraClient",
+    "blind_tool",
+    "BlindCrewAIAgent",
+    "BlindAutoGPTAgent",
+]
